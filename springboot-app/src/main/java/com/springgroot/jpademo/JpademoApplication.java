01c5d79bfcd3ea5e8b1b74ec1a09daf595395160
bineeshcertified <57544485+bineeshcertified@users.noreply.github.com>
2022-06-14 02:27:44 -0500

    Update JpademoApplication.java

--- springboot-app/src/main/java/com/springgroot/jpademo/JpademoApplication.java
+++ springboot-app/src/main/java/com/springgroot/jpademo/JpademoApplication.java
@@ -3,6 +3,7 @@ package com.springgroot.jpademo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import software.amazon.codeguruprofilerjavaagent.Profiler;
 
 @SpringBootApplication
 @EnableJpaAuditing
